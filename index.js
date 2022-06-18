1eb2d748026a9733007d13ec28ce26a66a22856f
Trillionclues <exceln646@gmail.com>
2022-06-18 06:03:22 +0100

    test

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-async function insertItems(url, table) {
-  const tHead = table.querySelector('thead')
-  const tBody = table.querySelector('tbody')
+async function getNames(url, table) {
+  const tableHead = table.querySelector('thead')
+  const tableBody = table.querySelector('tbody')
   const response = await fetch(url)
   const { headers, rows } = await response.json()
 
-  // clear the table
-  tHead.innerHTML = '<tr></tr>'
-  tBody.innerHTML = ''
+  // clear tables
+  tableHead.innerHTML = '<tr></tr>'
+  tableBody.innerHTML = ''
 
-  // populate table headers
-  for (const headerText of headers) {
+  // populate table header
+  headers.forEach(async (headerText) => {
     const headerElement = document.createElement('th')
-
     // console.log(headerElement)
     headerElement.textContent = headerText
-    tHead.querySelector('tr').appendChild(headerElement)
-  }
+    tableHead.querySelector('tr').appendChild(headerElement)
+  })
 
   // populate the rows
-  for (const row of rows) {
+  rows.forEach((row) => {
     const rowElement = document.createElement('tr')
     // console.log(rowElement)
-
-    // loop the nested rows
     for (const cellText of row) {
       const cellElement = document.createElement('td')
-
       cellElement.textContent = cellText
       rowElement.appendChild(cellElement)
+      tableBody.appendChild(rowElement)
     }
-
-    tBody.appendChild(rowElement)
-  }
+  })
 }
 
-insertItems('./data.json', document.querySelector('table'))
+getNames('./source/data.json', document.querySelector('table'))
